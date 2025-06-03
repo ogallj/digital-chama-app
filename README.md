9e8f1be0c2a40595fc29269b1d1da2ff36c304ea
Ogallo Joshua Odero <ogallo254@gmail.com>
2025-06-03 16:06:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# digital-chama-app
+Adigital chama system that automates group savings, sends contributions and disburses loan via mpesa reminder
---
+++ chama/src/test/java/com/chama/backend/ChamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chama.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
